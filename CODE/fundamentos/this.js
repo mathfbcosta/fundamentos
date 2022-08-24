1d8318e8823fd440d55563cfaf958056117eb778
Matheus Costa <105550843+mathfbcosta@users.noreply.github.com>
2022-08-24 15:17:38 -0400

    Update this.js

--- CODE/fundamentos/this.js
+++ CODE/fundamentos/this.js
@@ -10,9 +10,9 @@ aluno.imprimir()
 aluno2.imprimir()
 
 
-// function receberNomeDoMes(numero) {
-//     const mapeamento = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho',
-//         'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
-//     return mapeamento[--numero];
-// }
-// console.log(receberNomeDoMes(5))    
+   function receberNomeDoMes(numero) {
+       const mapeamento = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho',
+           'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
+       return mapeamento[--numero];
+   }
+   console.log(receberNomeDoMes(5))    
